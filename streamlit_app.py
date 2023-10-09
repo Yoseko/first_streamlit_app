72197d4f0724912602998c3d24feab33249e9f7d
ZYing <johnsonying1995@gmail.com>
2023-10-09 21:20:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ try:
   if not fruit_choice:
     streamlit.error('Please select a fruit to get informaiton.')
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     # take the json version of the response and normalize it
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     # output it on the screen as a table
